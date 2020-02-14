7bf247954e47f82ed9a7adca151cb9113210a2b5
Francisco <franciscodorset@hotmail.com>
2020-02-14 10:45:04 +0800

    changed previous and next Buttons to ImageButton

--- app/src/main/java/com/example/geoquiz/MainActivity.java
+++ app/src/main/java/com/example/geoquiz/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.geoquiz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
